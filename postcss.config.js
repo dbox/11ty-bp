a80cf997e07d4200bd8dd71aba90eb0a1d8aeada
dbox <danielbox@gmail.com>
2022-04-03 11:00:05 -0500

    postcss-preset-env works now

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('autoprefixer'),
+    require('postcss-preset-env')({ stage: 0 }),
   ],
 }
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,13 +1,13 @@
 module.exports = {
   isProduction: process.env.ELEVENTY_ENV === 'production',
-  title: 'Eleventy Starter',
-  description: 'A starter project built with Eleventy, Webpack and PostCSS.',
-  url: 'https://ess-eleventy-starter.netlify.app',
-  author: 'East Slope Studio',
+  title: 'Islesford Boatworks',
+  description: 'We are a 501(c)(3) non-profit community-based summer boatbuilding program for children ages 7 to 97 years young situated on Little Cranberry Island, Maine',
+  url: 'https://www.islesfordboatworks.org',
+  author: 'Daniel Box',
   meta: {
     default_social_image: '/static/images/social-image.jpg',
     facebook: 'https://facebook.com/eastslopestudio',
-    twitter: '@eastslopestudio',
+    twitter: '@dbox',
   },
   currentYear: new Date().getFullYear(),
 }
