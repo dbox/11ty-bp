bfe0233588732401c63d020e7cb2f034b98d036f
dbox <danielbox@gmail.com>
2022-04-11 21:39:59 -0500

    remove stuff

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,13 +1,13 @@
 module.exports = {
   isProduction: process.env.ELEVENTY_ENV === 'production',
-  title: 'Islesford Boatworks',
-  description: 'We are a 501(c)(3) non-profit community-based summer boatbuilding program for children ages 7 to 97 years young situated on Little Cranberry Island, Maine',
-  url: 'https://www.islesfordboatworks.org',
-  author: 'Daniel Box',
+  title: 'Site Title',
+  description: 'This is the site description',
+  url: 'https://www.kurtnoble.com',
+  author: 'Author Name',
   meta: {
     default_social_image: '/static/images/social-image.jpg',
     facebook: 'https://facebook.com/eastslopestudio',
-    twitter: '@dbox',
+    twitter: '@therealkni',
   },
   currentYear: new Date().getFullYear(),
 }
