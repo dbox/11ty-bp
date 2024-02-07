a208e5aaf463ae084d3dab1307807b0587743c7f
dbox <danielbox@gmail.com>
2024-02-07 08:24:06 -0500

    2.0 stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
+
 const htmlmin = require('html-minifier')
 
 module.exports = function (eleventyConfig) {
+
   // Copy static assets
   eleventyConfig.addPassthroughCopy('src/static')
 
@@ -24,6 +26,23 @@ module.exports = function (eleventyConfig) {
 
   // Watch changes to source assets that are compiled outside of 11ty
   eleventyConfig.addWatchTarget('./src/_assets/')
+
+  eleventyConfig.setServerOptions({
+    liveReload: true,
+    domDiff: true,
+    port: 8080,
+    watch: ["./dist/assets/*.css"],
+    showAllHosts: false,
+
+    https: {
+      // key: "./localhost.key",
+      // cert: "./localhost.cert",
+    },
+    encoding: "utf-8",
+
+    // Show the dev server version number on the command line
+    showVersion: true,
+  });
 
   return {
     dir: {
